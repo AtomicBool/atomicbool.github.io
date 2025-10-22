2859d04f5c7095ec2a29f9096600d07e0383c1ee
AtomicBool <84610772+AtomicBool@users.noreply.github.com>
2025-10-22 22:16:34 +0000

    Deploying to gh-pages from @ AtomicBool/atomicbool.github.io@fc071dccc511d28b6527c97b96cbcc5ec28bb4c2 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "AtomicBool/AtomicBool.github.io",
+    "data-repo": "AtomicBool/atomicbool.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
